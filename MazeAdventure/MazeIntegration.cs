7d181f33a0b119836880d7f982506a333e31b933
Fatemeh <Fatemeh@DESKTOP-FAJ8MGD>
2022-12-22 22:35:03 +0330

    Add MazeIntegration

---
+++ MazeAdventure/MazeIntegration.cs
@@ -0,0 +1,36 @@
+﻿using MazeAdventure.Contract;
+using MazeAdventure.Services;
+
+namespace MazeAdventure;
+public class MazeIntegration : IMazeIntegration
+{
+    private readonly MazeGeneratorService _mazeGeneratorService;
+    public MazeIntegration()
+    {
+        _mazeGeneratorService = new MazeGeneratorService();
+    }
+    public void BuildMaze(int size)
+    {
+        _mazeGeneratorService.BuildMaze(size);
+    }
+    public bool CausesInjury(int roomId)
+    {
+        return _mazeGeneratorService.CausesInjury(roomId);
+    }
+    public string GetDescription(int roomId)
+    {
+        return _mazeGeneratorService.GetDescription(roomId);
+    }
+    public int GetEntranceRoom()
+    {
+        return _mazeGeneratorService.GetEntranceRoom();
+    }
+    public int? GetRoom(int roomId, char direction)
+    {
+        return _mazeGeneratorService.GetRoom(roomId, direction);
+    }
+    public bool HasTreasure(int roomId)
+    {
+        return _mazeGeneratorService.HasTreasure(roomId);
+    }
+}
